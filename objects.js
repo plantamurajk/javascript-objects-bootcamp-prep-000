0ef8735136d4c6786874f7f89e61f1661466025f
John Plantamura <plantamurajk@gmail.com>
2019-06-21 15:27:30 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {Slowdive: 'Alison'
+}
+
+function updatePlaylist(playlist, artist, song){
+  var newPlaylist = Object.assign({}, playlist, {[artist]: song})
+  return newPlaylist
+}
+
